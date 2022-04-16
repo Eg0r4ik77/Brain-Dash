69e0b2faebb53f76309bb93f643aa090a32778a5
EgorSlesarenko <62872005+Eg0r4ik77@users.noreply.github.com>
2022-04-16 21:53:08 +0300

    finish game description fragment

--- app/src/main/java/com/example/androidgame/mainmenu/games/GameDescriptionFragment.java
+++ app/src/main/java/com/example/androidgame/mainmenu/games/GameDescriptionFragment.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.LayoutInflater;
@@ -16,14 +17,47 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.androidgame.R;
+import com.example.androidgame.gamecontrollers.Timer;
+import com.example.androidgame.mainmenu.games.calculationsmodegames.calculateexpressiongame.CalculateExpressionGame;
 
 public class GameDescriptionFragment extends Fragment {
 
+    private Timer timer;
+
+    private TextView timerText;
+    private TextView rulesText;
+
+    private Button startButton;
+
+    private Intent intent;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_game_description, container, false);
+        View view = inflater.inflate(R.layout.fragment_game_description, container, false);
+
+        intent = new Intent(getContext(), CalculateExpressionGame.class);
+
+        timerText = view.findViewById(R.id.transition_timer_text);
+        rulesText = view.findViewById(R.id.rules_text);
+        startButton = view.findViewById(R.id.start_calculate_expression_level);
+        timer = new Timer(4000, timerText) {
+            @Override
+            public void finish() {
+                startLevel();
+            }
+        };
+
+        startButton.setOnClickListener(v -> {
+            startButton.setClickable(false);
+            timer.run();
+        });
+
+        return view;
     }
 
     @Override
@@ -31,5 +65,13 @@ public class GameDescriptionFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
+    public void startLevel(){
+        try{
+            startActivity(intent);
+            getActivity().finish();
+        }catch (Exception exception){
+            Toast.makeText(getContext(), "Ошибка", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
